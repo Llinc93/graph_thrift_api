c87e39ecd00a79d2419192ec1eee93c67bb3e064
cpf <846859025@qq.com>
2020-03-31 11:01:42 +0800

    debug

--- search_subgraph_reverse.py
+++ search_subgraph_reverse.py
@@ -17,9 +17,9 @@ def print_cost_time(func):
     @functools.wraps(func)
     def inner(self, *args, **kwargs):
         start = time.time()
-        graph_index = func(self, *args, **kwargs)
+        number, graph_index = func(self, *args, **kwargs)
         end = time.time()
-        print(f'子图数量: {graph_index}\t耗时: {end - start}')
+        print(f'子图: {number}\t数量: {graph_index}\t耗时: {end - start}')
         return None
 
     return inner
@@ -33,8 +33,6 @@ class SearchSubgraphReverse(object):
     FILE = '/home/20200220csv/tmp/501/target/target_1.csv'
     ROOT = '/home/20200220csv/tmp'
     TMP = '/home/20200220csv/tmp/tmp_subgraph.csv'
-    if not os.path.exists(TMP):
-        os.makedirs(TMP)
 
     TARGET = '/home/20200220csv/tmp/sub_graph_1_1.csv'
 
@@ -60,39 +58,46 @@ class SearchSubgraphReverse(object):
         return None
 
     @print_cost_time
-    def search_1_1(self):
-        self.get_frequency_table()
-
-        # 查找1-1子图
+    def search_graph(self):
         records = filter(lambda x:self.frequency_table[x] == self.NUMBER, self.frequency_table.keys())
         count = 0
         for record in records:
             indexs = self.content_map[record]
             for index in indexs:
                 row = self.file_content[index]
-                if self.frequency_table[row[0]] + self.frequency_table[row[1]] == self.SUM:
+                if self.frequency_table[row[0]] + self.frequency_table[row[1]] == self.NUMBER + 1:
                     self.filter_indexs.add(index)
                     count += 1
+        return f'{self.NUMBER}_1', count
 
-        # todo 生成file文件
-        os.system(f'mv {self.FILE} {self.TMP}')
-        file = os.path.join(self.ROOT, 'search_subgraph_reverse.csv')
-        target_f = open(file, 'w', encoding='utf8')
-        writer = csv.writer(target_f)
+    def run(self):
+        self.get_frequency_table()
+        while self.NUMBER <= 200:
+            self.search_graph()
 
-        sub_graph = open(self.TARGET, 'w', encoding='utf8')
-        sub_writer = csv.writer(sub_graph)
-        for index, row in enumerate(self.file_content):
-            if index in self.filter_indexs:
-                sub_writer.writerow(row)
-            else:
-                writer.writerow(row)
+            # todo 生成file文件
+            os.system(f'mv {self.FILE} {self.TMP}')
+            file = os.path.join(self.ROOT, 'search_subgraph_reverse.csv')
+            target_f = open(file, 'w', encoding='utf8')
+            writer = csv.writer(target_f)
 
-        target_f.close()
-        sub_graph.close()
-        self.File = file
-        return count
+            sub_graph = open(self.TARGET, 'w', encoding='utf8')
+            sub_writer = csv.writer(sub_graph)
+            for index, row in enumerate(self.file_content):
+                if index in self.filter_indexs:
+                    sub_writer.writerow(row)
+                else:
+                    writer.writerow(row)
 
+            target_f.close()
+            sub_graph.close()
+            self.File = file
+            self.NUMBER += 1
+
+target_f.close()
+sub_graph.close()
+self.File = file
+self.NUMBER += 1
 
 if __name__ == '__main__':
     obj = SearchSubgraphReverse()
