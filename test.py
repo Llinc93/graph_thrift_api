f52d85606cc2b1356bb816272ec76016bf55ceb1
cpf <846859025@qq.com>
2020-04-09 11:22:47 +0800

    debug

--- test.py
+++ test.py
@@ -18,6 +18,7 @@ bra_links_set = set()
 
 files = [
     ('qiyexinxi.csv', 'ent_nodes'),
+    ('renyuanjiedian.csv', 'per_nodes'),
     ('ziranrentouzi.csv', 'per'),
     ('qiyetouzi.csv', 'ent_links'),
     ('qiyefenzhi.csv', 'bra'),
@@ -28,22 +29,28 @@ for file, label in files:
         with open(file) as f:
             reader = csv.reader(f)
             for row in f:
-                ent_nodes.extend(row)
-                ent_nodes_set |= set(row)
+                ent_nodes.extend(row[-1])
+                ent_nodes_set |= set(row[-1])
+    elif label == 'per_nodes':
+        with open(file) as f:
+            reader = csv.reader(f)
+            for row in f:
+                per_nodes.extend(row[-1])
+                per_nodes_set |= set(row[-1])
     elif label == 'per':
         with open(file) as f:
             reader = csv.reader(f)
             for row in f:
-                per_nodes.extend(row[0])
-                per_nodes_set |= set(row[0])
                 per_links.extend(row[0])
                 per_links_set |= set(row[0])
     elif label == 'ent_links':
         with open(file) as f:
             reader = csv.reader(f)
             for row in f:
-                ent_links.extend(row)
-                ent_links_set |= set(row)
+                ent_links.append(row[0])
+                ent_links.append(row[-1])
+                ent_links_set.add(row[0])
+                ent_links_set.add(row[-1])
     elif label == 'bra':
         with open(file) as f:
             reader = csv.reader(f)
